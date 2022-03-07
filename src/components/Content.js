7833f226ecd0b3340197ba1a9a931152b19b5dfc
isabelanery <isabelanery242@gmail.com>
2022-03-07 18:50:02 -0300

    adds requirement 2

--- src/components/Content.js
+++ src/components/Content.js
@@ -1,5 +1,5 @@
 import React from 'react';
-import { Route } from 'react-router-dom';
+import { Route, Switch } from 'react-router-dom';
 import Login from '../pages/Login';
 import Search from '../pages/Search';
 import Album from '../pages/Album';
@@ -12,13 +12,15 @@ class Content extends React.Component {
   render() {
     return (
       <main>
-        <Route exact path="/" component={ Login } />
-        <Route path="/search" component={ Search } />
-        <Route path="/album/:id" component={ Album } />
-        <Route path="/favorites" component={ Favorites } />
-        <Route path="/profile" component={ Profile } />
-        <Route path="/profile/edit" component={ ProfileEdit } />
-        <Route path="*" component={ NotFound } />
+        <Switch>
+          <Route exact path="/" component={ Login } />
+          <Route path="/search" component={ Search } />
+          <Route path="/album/:id" component={ Album } />
+          <Route path="/favorites" component={ Favorites } />
+          <Route exact path="/profile" component={ Profile } />
+          <Route path="/profile/edit" component={ ProfileEdit } />
+          <Route component={ NotFound } />
+        </Switch>
       </main>
     );
   }
--- src/pages/Login.js
+++ src/pages/Login.js
@@ -1,13 +1,91 @@
 import React from 'react';
+import PropTypes from 'prop-types';
+import Loading from '../components/Loading';
+import { createUser } from '../services/userAPI';
 
 class Login extends React.Component {
+  constructor() {
+    super();
+
+    this.state = {
+      nameUser: '',
+      disableBtn: true,
+      loading: false,
+    };
+
+    this.handleClick = this.handleClick.bind(this);
+  }
+
+  handleChange = ({ target }) => {
+    const { name, value } = target;
+    const minLength = 3;
+    const verifyName = value.length < minLength;
+
+    this.setState({
+      [name]: value,
+      disableBtn: verifyName,
+    });
+  }
+
+  async handleClick(event) {
+    event.preventDefault();
+    const { nameUser } = this.state;
+
+    this.setState({ loading: true });
+
+    console.log(createUser);
+    await createUser({
+      name: nameUser,
+      email: '',
+      image: '',
+      description: '',
+    });
+
+    // this.setState({ loading: false });
+
+    const { history } = this.props;
+
+    history.push('/search');
+  }
+
   render() {
+    const { nameLogin, disableBtn, loading } = this.state;
+
     return (
-      <div data-testid="page-login">
-        Login
-      </div>
+      <main>
+
+        {
+          loading
+            ? <Loading />
+            : (
+              <div data-testid="page-login">
+                <form>
+                  <input
+                    data-testid="login-name-input"
+                    type="text"
+                    name="nameUser"
+                    onChange={ this.handleChange }
+                    value={ nameLogin }
+                  />
+                  <button
+                    data-testid="login-submit-button"
+                    type="submit"
+                    disabled={ disableBtn }
+                    onClick={ this.handleClick }
+                  >
+                    Entrar
+                  </button>
+                </form>
+              </div>)
+        }
+
+      </main>
     );
   }
 }
 
 export default Login;
+
+Login.propTypes = {
+  history: PropTypes.element.isRequired,
+};
