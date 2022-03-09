4beb91b5a605ef84bf9e091e324f5e6c40dce7f5
isabelanery <isabelanery242@gmail.com>
2022-03-09 10:28:04 -0300

    adds requirement 13.

--- src/pages/Profile.js
+++ src/pages/Profile.js
@@ -1,10 +1,67 @@
 import React from 'react';
+import { Link } from 'react-router-dom';
+import Loading from '../components/Loading';
+import { getUser } from '../services/userAPI';
 
 class Profile extends React.Component {
+  constructor() {
+    super();
+
+    this.state = {
+      loading: false,
+      user: undefined,
+    };
+
+    this.getUserInfo = this.getUserInfo.bind(this);
+  }
+
+  componentDidMount() {
+    this.getUserInfo();
+  }
+
+  async getUserInfo() {
+    this.setState({ loading: true });
+
+    const data = await getUser();
+    this.setState({
+      user: data,
+      loading: false,
+    });
+  }
+
   render() {
+    const { loading, user } = this.state;
+
     return (
       <div data-testid="page-profile">
-        Profile
+        {
+          loading ? <Loading />
+            : user
+              && (
+                <section className="userInfo">
+                  <p>
+                    { user.name }
+                  </p>
+
+                  <p>
+                    { user.email }
+                  </p>
+
+                  <p>
+                    { user.description }
+                  </p>
+
+                  <img
+                    src={ user.image }
+                    alt={ user.name }
+                    data-testid="profile-image"
+                  />
+
+                  <Link to="/profile/edit">Editar perfil</Link>
+                </section>
+
+              )
+        }
       </div>
     );
   }
