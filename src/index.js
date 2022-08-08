66e2b10881687a0983556f0a09b42a367068674e
isabelanery <isabelanery242@gmail.com>
2022-08-08 15:23:38 -0300

    feat: working on deploy

--- src/index.js
+++ src/index.js
@@ -5,7 +5,7 @@ import App from './App';
 import './index.css';
 
 ReactDOM.render(
-  <BrowserRouter>
+  <BrowserRouter basename={process.env.PUBLIC_URL}>
     <App />
   </BrowserRouter>,
   document.getElementById('root'),
